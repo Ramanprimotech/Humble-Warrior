81113d4f576469748903e4b7597342aef2a7e0fc
apple <adil.ayoub@primotech.com>
2023-02-08 16:50:41 +0530

    Home Options Tab View

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:humble_warrior/utils/app_colors.dart';
 import 'package:humble_warrior/utils/app_icons.dart';
 import 'package:humble_warrior/utils/app_text.dart';
 import 'package:humble_warrior/utils/routes/app_routes.dart';
@@ -17,7 +16,6 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          elevation: 0,
           centerTitle: false,
           title: CustomSearchBar(
             focusNode: controller.focusNode,
@@ -34,9 +32,17 @@ class HomeScreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
                 height: 8,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: AppText(
+                  "Categories",
+                  fontSize: 24,
+                ),
               ),
               SizedBox(
                 height: 70,
@@ -64,100 +70,110 @@ class HomeScreen extends StatelessWidget {
                       );
                     }),
               ),
-              Divider(
-                color: AppColors.grey,
-                height: 2,
-              ),
-              Expanded(
-                child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    itemCount: HomeOptions.homeOptionsList.length,
-                    itemBuilder: (ctx, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: GestureDetector(
-                          onTap: () {
-                            Get.toNamed(AppRoutes.homeOptions,
-                                arguments: <int>[index]);
-                          },
-                          child: Stack(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(15),
-                                child: Image.network(
-                                  HomeOptions.homeOptionsList[index].image,
-                                  fit: BoxFit.fitWidth,
-                                  height: 200,
-                                  alignment: Alignment.topCenter,
-                                  width: Get.width,
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 20,
-                                left: (Get.width - 250) / 2,
-                                right: (Get.width - 250) / 2,
-                                child: Container(
-                                  height: 40,
-                                  width: 250,
-                                  alignment: Alignment.center,
-                                  decoration:
-                                      BoxDecoration(color: Colors.white),
-                                  child: AppText(
-                                    HomeOptions.homeOptionsList[index].text,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-              ),
               const SizedBox(
-                height: 15,
+                height: 8,
               ),
-              Row(
-                children: [
-                  const AppText(
-                    "BRAND'S I",
-                    fontSize: 24,
-                  ),
-                  AppIcons.heart(
-                    iconColor: Colors.red,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: 80,
-                width: Get.height,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 25,
-                    itemBuilder: (ctx, index) {
-                      return SizedBox(
-                        height: 80,
-                        width: 120,
-                        child: Column(
-                          children: [
-                            Image.network(
-                              alignment: Alignment.bottomCenter,
-                              controller.brands[index % 4],
-                              fit: BoxFit.contain,
-                              height: 80,
-                              width: 110,
-                            ),
-                          ],
+              Flexible(
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: AppText(
+                        "Deals",
+                        fontSize: 24,
+                      ),
+                    ),
+                    ...HomeOptions.homeOptionsList
+                        .map<Widget>(
+                            (e) => homeOption(homeOptions: e, index: 1))
+                        .toList(),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        const AppText(
+                          "BRAND'S I",
+                          fontSize: 24,
                         ),
-                      );
-                    }),
-              ),
+                        AppIcons.heart(
+                          iconColor: Colors.red,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 80,
+                      width: Get.height,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 25,
+                          itemBuilder: (ctx, index) {
+                            return SizedBox(
+                              height: 80,
+                              width: 120,
+                              child: Column(
+                                children: [
+                                  Image.network(
+                                    alignment: Alignment.bottomCenter,
+                                    controller.brands[index % 4],
+                                    fit: BoxFit.contain,
+                                    height: 80,
+                                    width: 110,
+                                  ),
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget homeOption({required HomeOptions homeOptions, required int index}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8),
+    child: GestureDetector(
+      onTap: () {
+        Get.toNamed(AppRoutes.homeOptions, arguments: <int>[index]);
+      },
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              homeOptions.image,
+              fit: BoxFit.fitWidth,
+              height: 200,
+              alignment: Alignment.topCenter,
+              width: Get.width,
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: (Get.width - 250) / 2,
+            right: (Get.width - 250) / 2,
+            child: Container(
+              height: 40,
+              width: 250,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(color: Colors.white),
+              child: AppText(
+                homeOptions.text,
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
