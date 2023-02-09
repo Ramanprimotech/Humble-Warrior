f869d08a1b8432d00e5d54c792fad64392e0393d
apple <adil.ayoub@primotech.com>
2023-02-09 17:52:46 +0530

    API's

--- lib/utils/search_bar/search_bar_UI.dart
+++ lib/utils/search_bar/search_bar_UI.dart
@@ -26,27 +26,30 @@ class CustomSearchBar extends StatefulWidget {
 class _CustomSearchBarState extends State<CustomSearchBar> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoSearchTextField(
-      focusNode: widget.focusNode,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          border: Border.all(color: AppColors.grey, width: 1.5)),
-      controller: widget.textEditingController,
-      placeholder: "Search",
-      prefixInsets: const EdgeInsets.only(left: 15, right: 10),
-      suffixInsets: const EdgeInsets.only(right: 15),
-      suffixMode: OverlayVisibilityMode.editing,
-      prefixIcon: Image.asset(
-        ImagePathAssets.hw_logo,
-        height: 48,
-        width: 48,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: CupertinoSearchTextField(
+        focusNode: widget.focusNode,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(color: AppColors.grey, width: 1.5)),
+        controller: widget.textEditingController,
+        placeholder: "Search",
+        prefixInsets: const EdgeInsets.only(left: 15, right: 10),
+        suffixInsets: const EdgeInsets.only(right: 15),
+        suffixMode: OverlayVisibilityMode.editing,
+        prefixIcon: Image.asset(
+          ImagePathAssets.hw_logo,
+          height: 40,
+          width: 40,
+        ),
+        onChanged: (String? text) {},
+        onSubmitted: widget.onSubmit ?? (String? text) {},
+        onTap: widget.onTap ??
+            () {
+              debugPrint("taped");
+            },
       ),
-      onChanged: (String? text) {},
-      onSubmitted: widget.onSubmit ?? (String? text) {},
-      onTap: widget.onTap ??
-          () {
-            debugPrint("taped");
-          },
     );
   }
 }
--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -1,24 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:humble_warrior/utils/image_path_assets.dart';
 import 'package:humble_warrior/view/splash/spalsh_controller.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     SplashController controller = Get.find();
-   return SafeArea(
-     child: Scaffold(
-       body: Center(
-         child: Image.asset(ImagePathAssets.hw_logo,height: 200,width: 200,),
-       ),
-
-     ),
-   );
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Center(
+            child: Image.asset(
+              ImagePathAssets.hw_logo,
+              height: 200,
+              width: 200,
+            ),
+          ),
+        ),
+      ),
+    );
   }
-
-
 }
