4f900cd147883614a8767abc5f83d43ca56ce832
apple <adil.ayoub@primotech.com>
2023-02-13 12:32:14 +0530

    Product API

--- lib/view/bottom_navigation/bottom_navigation_binging.dart
+++ lib/view/bottom_navigation/bottom_navigation_binging.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
-import 'package:humble_warrior/utils/app_themes/app_theme_controller.dart';
 import 'package:humble_warrior/view/bottom_navigation/bottom_navigation_controller.dart';
-import 'package:humble_warrior/view/home/home_controller.dart';
+import 'package:humble_warrior/view/home/home_controllerrt';
 import 'package:humble_warrior/view/productDetail/product_detail_controller.dart';
 import 'package:humble_warrior/view/wish_list/wish_list_controller.dart';
 
