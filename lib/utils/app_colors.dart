8421d6742c32192fa0cc4adb46c05c5c448c8971
Munish <Munish.primotech@gmail.com>
2023-03-17 12:33:52 +0530

    refactor code.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color lightPurple = Color(0xFF8C64FF);
   static const Color purple = Color(0xFF443266);
   static const Color blue = Color(0xff36A9C2);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static Color secondary = Colors.red.shade300;
-  static const Color scafoldBackground = Color(0xFFF5F5F5);
   static const Color backgroundDark = Color(0xFF2D2D2D);
 
   static Color gray = Colors.grey.shade700;
@@ -16,16 +14,11 @@ class AppColors {
   static Color primary = const Color(0xFFE5A1D2);
   static const Color facebookClr = Color(0xFF1877F2);
   static Color twitterClr = Colors.blue.shade400;
-  static Color phoneClr = Colors.blue.shade600;
-  static const Color whatsClr = Color(0xFF018F99);
-  static Color green = Colors.green.shade600;
-  static Color yellow = Colors.yellow.shade600;
   static Color red = Colors.red.shade600;
   static Color pink = Colors.pink;
   static const Color shadowDark = Colors.black54;
   static const Color shadowLight = Color(0xFFD8D4CF);
   static const Color readBox = Color(0xFFDAEBFD);
-  // static const Color switchActiveColor = Color(0xFF4CD864);
   static const Color switchActiveColor = Color(0xFF2af500);
   static const Color switchInactiveColor = Color(0xFF494E4A);
   static const Color appGreen = Color(0xFF63F3B8);
--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -4,21 +4,6 @@ class AppStrings {
   static const String er403 = "Forbidden : Error 403";
   static const String er500 = "Internal Server Error : Error 500";
   static const String errDefault = "Something went wrong";
-  static const String rupees = "₹";
-  static const String degree = "°";
-  static const String skipTxt = "SKIP";
-  static const String nextTxt = "NEXT";
-  static const String startTxt = "START";
-  static const String humbleWarriorTxt = "The Humble Warrior";
-  static const String fashionLifestyleTxt = "Fashion & Lifestyle for women";
-  static const String lookGoodTxt = "Look Good";
-  static const String feelGoodTxt = "Feel Good";
-  static const String myWishListTxt = "My Wishlist";
-  static const String doGoodTxt = "Do Good";
-  static const String tmTxt = "TM";
-
-  static const String loremText =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...";
   static const String lorem =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 }
@@ -38,35 +23,34 @@ const String photoViewerTxt = "Photo Viewer";
 
 /*===================================================Home page strings========================================================*/
 
-const String beautyTxt = "Beauty";
-const String menTxt = "Men";
-const String giftTxt = "Gift";
-const String giftForMomTxt = "Gift for Mom";
-const String healthTxt = "Health";
-const String kidsTxt = "Kids";
-const String foodTxt = "Food";
-const String kitchenTxt = "Kitchen";
-const String womensFashionTxt = "Women's Fashion";
-const String outdoorTxt = "Outdoor";
-const String travelTxt = "Travel";
-const String packingTxt = "Packing";
-const String fitnessTxt = "Fitness";
-const String jewelryTxt = "Jewelry";
-const String babyTxt = "Baby";
-const String booksTxt = "Books";
-const String vehiclesTxt = "Vehicles";
-const String petsTxt = "Pets";
+// const String menTxt = "Men";
+// const String giftTxt = "Gift";
+// const String giftForMomTxt = "Gift for Mom";
+// const String healthTxt = "Health";
+// const String kidsTxt = "Kids";
+// const String foodTxt = "Food";
+// const String kitchenTxt = "Kitchen";
+// const String womensFashionTxt = "Women's Fashion";
+// const String outdoorTxt = "Outdoor";
+// const String travelTxt = "Travel";
+// const String packingTxt = "Packing";
+// const String fitnessTxt = "Fitness";
+// const String jewelryTxt = "Jewelry";
+// const String babyTxt = "Baby";
+// const String booksTxt = "Books";
+// const String vehiclesTxt = "Vehicles";
+// const String petsTxt = "Pets";
 const String brandsTxt = "BRAND'S";
 const String viewAllTxt = "VIEW ALL";
-const String nikeTxt = "Nike";
-const String adidasTxt = "Adidas";
-const String reebokTxt = "Reebok";
-const String gucciTxt = "Gucci";
+// const String nikeTxt = "Nike";
+// const String adidasTxt = "Adidas";
+// const String reebokTxt = "Reebok";
+// const String gucciTxt = "Gucci";
 const String donnaDailyDealTxt = "Donna's Daily Deals";
 const String donnaFavouriteDealTxt = "Donna's Favourite Deals";
 const String donnaFrontPageDealTxt = "Front Page Deals";
 
-const String brandLink = "https://humblewarrior.com/";
+// const String brandLink = "https://humblewarrior.com/";
 
 /*===================================================My Account Page strings========================================================*/
 
@@ -83,8 +67,8 @@ const String helpSupportTxt = "Help & Support";
 const String termsConditionsTxt = "Terms & Conditions";
 const String login = "Login";
 const String logoutTxt = "Logout";
-const String appVersionTxt = "APP VERSION";
-const String addPhotoTxt = "Add Photo";
+// const String appVersionTxt = "APP VERSION";
+// const String addPhotoTxt = "Add Photo";
 
 /*===================================================About Donna Page strings========================================================*/
 
@@ -106,13 +90,10 @@ const String lookGoodTxt = "Look Good";
 const String feelGoodTxt = "Feel Good";
 const String doGoodTxt = "Do Good";
 const String tmTxt = "TM";
-const String subscribe = "SUBSCRIBE";
 
 /*===================================================Home Option Page strings========================================================*/
 
 const String shopNowTxt = "SHOP NOW";
-const String hmTxt = "HM";
-const String somethingTxt = "Something Went Wrong";
 const String loadingDealsTxt = " Loading Deals for you...";
 const String noDataFoundTxt = "No Data Found";
 const String donnasOptionTxt = "Donna's Options";
@@ -122,24 +103,24 @@ const String donnasOptionTxt = "Donna's Options";
 const String cameraTxt = "Camera";
 const String galleryTxt = "Gallery";
 const String cancelTxt = "Cancel";
-const String userData = "userData";
+// const String userData = "userData";
 const String userEmail = "userEmail";
 const String userName = "userName";
 const String userPhoneNumber = "userPhoneNumber";
 const String userProfilePic = "userProfilePic";
-const String userId = "userId";
-const String showMoreTxt = 'Show more';
-const String showLessTxt = 'Show less';
+// const String userId = "userId";
+// const String showMoreTxt = 'Show more';
+// const String showLessTxt = 'Show less';
 const String readlessTxt = 'read less';
 const String readmoreTxt = 'read more';
 const String accessingMsgTxt = 'Please Login for accessing this feature.';
-const String webSiteError = 'Unable to reach the website';
+// const String webSiteError = 'Unable to reach the website';
 const String gotoLoginTxt = 'Go to Login';
 const String retryTxt = 'Retry';
-const String myFvrtTxt = 'My Favourite';
+// const String myFvrtTxt = 'My Favourite';
 const String buyNowTxt = 'Buy Now';
 const String sortTxt = "Sort";
-const String notNowTxt = "Not Now";
+// const String notNowTxt = "Not Now";
 const String loginWishTxt = "Login to add items to wishlist";
 
 /*===================================================Shared Preference========================================================*/
