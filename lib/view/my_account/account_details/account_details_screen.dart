54df50eb44ac500fc32a6e083e1665df7ec1a031
bijay <bijay.mandal@primotech.com>
2023-03-17 16:08:27 +0530

    remove migrate

--- lib/view/my_account/account_details/account_details_screen.dart
+++ lib/view/my_account/account_details/account_details_screen.dart
@@ -21,63 +21,68 @@ class AccountDetails extends StatelessWidget {
     AccountDetailsController accountController = Get.find();
     return Scaffold(
       bottomNavigationBar: bottomNavigationWidget(context),
-      body: SafeArea(
-          child: Obx(
+      body: Obx(
         () => accountController.userCheck.value == false
-            ? Column(
-                children: [
-                  CommonWidgets.titleBar(context,
-                      title: accountDetailsTxt, fontSize: 20),
-                  150.sh,
-                  loginFirst(context),
-                ],
-              )
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: Get.width,
-                    height: Get.height / 3,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                      fit: BoxFit.fill,
-                      opacity: 0.7,
-                      image: AssetImage(ImagePathAssets.backgroundImg),
-                    )),
-                    child: Column(
-                      children: [
-                        CommonWidgets.titleBar(context,
-                            title: accountDetailsTxt, fontSize: 20),
-                        profileImg(),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          50.sh,
-                          heading(headingTitle: "Name"),
-                          5.sh,
-                          subTitle(title: accountController.username),
-                          20.sh,
-                          heading(headingTitle: "Email"),
-                          5.sh,
-                          subTitle(title: accountController.user),
-                          20.sh,
-                          heading(headingTitle: 'Phone Number'),
-                          5.sh,
-                          subTitle(
-                              title: accountController.userPhone.toString()),
-                          20.sh,
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+        ? SafeArea(
+          child: Column(
+              children: [
+                // 50.sh,
+                CommonWidgets.titleBar(context,
+                    title: accountDetailsTxt, fontSize: 20),
+                150.sh,
+                loginFirst(context),
+              ],
+            ),
+        )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: Get.width,
+                height: Get.height / 2.5,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  fit: BoxFit.fill,
+                  opacity: 0.7,
+                  image: AssetImage(ImagePathAssets.backgroundImg),
+                )),
+                child: Column(
+                  children: [
+                    50.sh,
+                    CommonWidgets.titleBar(context,
+                        title: accountDetailsTxt, fontSize: 20),
+                    Spacer(),
+                    profileImg(),
+                    Spacer(),
+                  ],
+                ),
               ),
-      )),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      50.sh,
+                      heading(headingTitle: "Name"),
+                      5.sh,
+                      subTitle(title: accountController.username),
+                      20.sh,
+                      heading(headingTitle: "Email"),
+                      5.sh,
+                      subTitle(title: accountController.user),
+                      20.sh,
+                      heading(headingTitle: 'Phone Number'),
+                      5.sh,
+                      subTitle(
+                          title: accountController.userPhone.toString()),
+                      20.sh,
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+      ),
     );
   }
 
