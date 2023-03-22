d99e63ff62ba3615f4d2c72c06333f65f88bfbb0
apple <adil.ayoub@primotech.com>
2023-03-22 18:55:11 +0530

    Bug Sheet

--- lib/utils/common/html.dart
+++ lib/utils/common/html.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:html/dom.dart' as dom;
 import 'package:humble_warrior/utils/common/common_functionality.dart';
-import 'package:simple_html_css/simple_html_css.dart';
+import 'package:humble_warrior/utils/extensions.dart';
+// import 'package:simple_html_css/simple_html_css.dart';
+// import 'package:simple_html_css/simple_html_css.dart';
 
 class HtmlData {
   htmlString(BuildContext context, title) {
-    return HTML.toRichText(context, title,
-        defaultTextStyle: TextStyle(overflow: TextOverflow.ellipsis),
-        linksCallback: (dynamic link) {
-      CommonUtils().urlLauncher(url: link.toString());
-    });
+    // var document = parse(title);
+    // var imgList = document.querySelectorAll("img");
+
+    // for (dom.Element img in imgList) {
+    //   print(img.attributes["src"]);
+    //   print(img.toString());
+    // }
+    // return HTML.toRichText(context, title,
+    //     defaultTextStyle: TextStyle(overflow: TextOverflow.ellipsis),
+    //     linksCallback: (dynamic link) {
+    //   CommonUtils().urlLauncher(url: link.toString());
+    // });
+
+    /// sanitize or query document here
+    Widget html = Html(
+        data: title,
+        onLinkTap: (String? url, RenderContext context,
+            Map<String, String> attributes, dom.Element? element) {
+          CommonUtils().urlLauncher(url: url.toString());
+        },
+        style: {
+          "body": Style(
+              padding: 0.ph,
+              color: Theme.of(context).textTheme.displayMedium!.color),
+          // tables will have the below background color
+        });
+    return html;
   }
 }
